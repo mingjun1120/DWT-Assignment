10a09ddca0fe7877712e5459d7ee7703c60b555e
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2021-04-15 02:41:05 +0800

    Update Analysis(lcc).sql

--- Analysis(lcc).sql
+++ Analysis(lcc).sql
@@ -1,4 +1,20 @@
+SET linesize 115
+SET pagesize 240
 CLEAR COLUMNS
 CLEAR BREAKS
 CLEAR COMPUTES
+CLEAR BUFFER
 TTITLE OFF
+
+ACCEPT yearInput DATE FORMAT 'YYYY'-
+PROMPT 'Enter year: '
+
+ACCEPT city CHAR FORMAT A25-
+PROMPT 'Select city: '
+
+SELECT DISTINCT(R.rest_city) AS City
+FROM DIM_Restaurant R, sales_fact S
+WHERE S.date_key IN (SELECT date_key FROM dim_date
+                     WHERE cal_Year = '&yearInput'
+                    ) AND R.restaurant_key = S.restaurant_key
+ORDER BY R.rest_city;
